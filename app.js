25a0785af9f36b8b02dd1bb58dafb0a7c5cccfde
schwarzblade <you@example.com>
2020-04-26 12:56:35 +0200

    expenses list

--- app.js
+++ app.js
@@ -65,14 +65,52 @@ class UI {
 		const expenseValue = this.expenseInput.value;
 		const amountValue = this.amountInput.value;
 	
-		if(expenseValue === '' || amountValue === '' || amountValue < 0)
+		if(expenseValue === '' || amountValue === '' || amountValue < 0){
 			this.expenseFeedback.classList.add("showItem");
 			this.expenseFeedback.innerHTML = `<p>not empty, negtive </p>`;
-			const self = this;
-			setTimeout(() =>{
-				this.expenseFeedback.classList.remove("showItem");
-			},3000)
+			setTimeout(() => this.expenseFeedback.classList.remove("showItem"),3000);
+		} else {
+			let amount = parseInt(amountValue);
+			this.expenseInput.value = '';
+			this.amountInput.value = '';
+
+			let expense = {
+				id: this.itemID,
+				title: expenseValue,
+				cost: amount
+			}
+
+			this.itemID++;
+			this.itemList.push(expense);
+			this.addExpense(expense);
+			//show balance
 		}
+
+	}
+
+
+	addExpense(expense){
+		const div = document.createElement('div');
+		div.classList.add('expense');
+		div.innerHTML = `
+			<div class="expense-item d-flex justify-content-between align-items-baseline">
+
+				<h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+				<h5 class="expense-amount mb-0 list-item">${expense.cost}</h5>
+				<div class="expense-icons list-item">
+
+				<a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+					<i class="fas fa-edit"></i>
+				</a>
+				<a href="#" class="delete-icon" data-id="${expense.id}">
+					<i class="fas fa-trash"></i>
+				</a>
+				</div>
+				</div>
+`;
+	this.expenseList.appendChild(div);
+
+	}
 
 }
 
