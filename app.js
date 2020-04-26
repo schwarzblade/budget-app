3efdc3e03020d6e305cfce872ae033c30363c99a
schwarzblade <you@example.com>
2020-04-26 10:23:32 +0200

    expense form(valid)

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ class UI {
 		this.balanceAmount = document.getElementById("balance-amount");
 		this.expenseForm = document.getElementById("expense-form");
 		this.expenseInput = document.getElementById("expense-input");
-		this.expenseList = document.querySelector(".expense-list");
+		this.amountInput = document.getElementById("amount-input");
+		this.expenseList = document.getElementById("expense-list");
 		this.itemList = [];
 		this.itemID = 0;
 	}
@@ -29,21 +30,51 @@ class UI {
 			this.budgetAmount.textContent = value;
 			this.budgetInput.value = "";
 			this.shwoBalance();
+		
 		}
 
 	}
 
 	shwoBalance(){
 		const expenses = this.totalExpenses();
+		const total = parseInt(this.budgetAmount.textContent) - expenses;
+		this.balanceAmount.textContent = total;
+		if(total < 0){
+			this.balance.classList.remove("showGreen","showBlack");
+			this.balance.classList.add("showRed");
+		} else if(total > 0){
+			this.balance.classList.remove("showRed","showBlack");
+			this.balance.classList.add("showGreen");
+		} else {
+			this.balance.classList.remove("showGreen","showRed");
+			this.balance.classList.add("showBlack");
+		}
+
+
 
 	}
 
 
 	totalExpenses(){
-		
+		let total = 400;
+		return total;
 	}
-}
 
+
+	submitExpenseForm(){
+		const expenseValue = this.expenseInput.value;
+		const amountValue = this.amountInput.value;
+	
+		if(expenseValue === '' || amountValue === '' || amountValue < 0)
+			this.expenseFeedback.classList.add("showItem");
+			this.expenseFeedback.innerHTML = `<p>not empty, negtive </p>`;
+			const self = this;
+			setTimeout(() =>{
+				this.expenseFeedback.classList.remove("showItem");
+			},3000)
+		}
+
+}
 
 
 
@@ -65,6 +96,7 @@ function eventListeners(){
 	})
 	expenseForm.addEventListener('submit', (e) => {
 		e.preventDefault();
+		ui.submitExpenseForm();
 	})
 	expenseList.addEventListener('click', () => {
 		
